20091030b6081b1cee903e4fe3eaf242e5ea7601
Martin Stein <stein@openinc.de>
2026-01-05 13:15:55 +0100

    fix: remove .ts from import

--- packages/core/src/locale/index.ts
+++ packages/core/src/locale/index.ts
@@ -4,7 +4,7 @@ import es from "./es";
 import hi from "./hi";
 import ru from "./ru";
 import zh_tw from "./zh_tw";
-import de from "./de.ts";
+import de from "./de";
 import { Context } from "..";
 
 const localeObj: Record<string, typeof zh> = {
